d78a31e3afb3bef18fbee52188f21b1c6e5937ae
JesusFinda <jesusfinda13@gmail.com>
2023-11-22 12:04:15 +0100

    Criando tabela candidato

--- src/main/java/ao/com/jesusfinda/gestao_vagas/modules/canditate/CandidateEntity.java
+++ src/main/java/ao/com/jesusfinda/gestao_vagas/modules/canditate/CandidateEntity.java
@@ -4,16 +4,28 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate") //Criar uma tabela através da entidade(class)
 public class CandidateEntity {
     
+    /**
+     *
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) //Anotion
     private UUID id;
 
+    public CandidateEntity() {
+    }
     @NotBlank
     private String name;
 
